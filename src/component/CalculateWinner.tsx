ef7f4ce55e9e7dcdd4b808a3eac6433b54141355
mika <mks4sp.gniward@gmail.com>
2022-09-15 02:56:26 +0000

    CalculateWinner.tsxをfunctionに戻す

--- src/component/CalculateWinner.tsx
+++ src/component/CalculateWinner.tsx
@@ -1,30 +1,5 @@
 import { Icon } from "../interface";
 
-interface WinnerProps {
-  squares: Icon[];
-}
-
-const calculateWinner: React.FC<WinnerProps> = (squares) => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-};
-export default calculateWinner;
-/*
 export default function calculateWinner(squares: Array<Icon>) {
   const lines = [
     [0, 1, 2],
@@ -43,4 +18,4 @@ export default function calculateWinner(squares: Array<Icon>) {
     }
   }
   return null;
-}*/
+}
