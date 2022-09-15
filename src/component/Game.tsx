d79c7e1c30078ec6227569028333989ddc00c818
mika <mks4sp.gniward@gmail.com>
2022-09-15 07:01:04 +0000

    Game.tsx書き換え

--- src/component/Game.tsx
+++ src/component/Game.tsx
@@ -3,194 +3,70 @@ import { Icon } from "../interface";
 import Board from "./Board";
 import CalculateWinner from "./CalculateWinner";
 
-interface GameProps {
-  history: {
-    squares: Icon[];
-  }[];
-  stepNumber: number;
-  xIsNext: boolean;
+interface History {
+  squares: Icon[];
 }
-//<{}>：暗黙的な型推論が行われる
-class Game extends React.Component<{}, GameProps> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null)
-        }
-      ],
-      stepNumber: 0, //いま何手目の状態を見ているのか
-      xIsNext: true
-    };
-  }
 
-  handleClick(i: number) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
+const Game: React.FC = () => {
+  const [history, setHistory] = useState<History>([
+    { squares: Array(9).fill(null) }
+  ]);
+  const [stepNumber, setStepNumber] = useState<number>(0);
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+
+  //クリックしたら値を書き換える
+  const handleClick = (i: number) => {
+    const history_s = history.slice(0, stepNumber + 1);
+    const current = history_s[history_s.length - 1];
     const squares = current.squares.slice(); //squares配列のコピーを作成
+
+    //ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合
     if (CalculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      //concat=元の配列をミューテートしないpush
-      history: history.concat([
-        {
-          squares: squares
-        }
-      ]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
-    });
-  }
 
-  jumpTo(step: number) {
-    this.setState({
-      //setState で直接指定されたプロパティのみを更新
-      stepNumber: step,
-      xIsNext: step % 2 === 0
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = CalculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i: number) => this.handleClick(i)} //Boardに関数渡す
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-export default Game;
-
-/*type Props = {
-  history: {
-    squares: string;
-  }[];
-  stepNumber: number;//いま何手目の状態を見ているのか
-  xIsNext: boolean;
-};*/
-
-/*const Game: React.FC = () => {
-  /*
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null)
-        }
-      ],
-      stepNumber: 0, //いま何手目の状態を見ているのか
-      xIsNext: true
-    };
-  }*/
-/*  const [state, setState] = useState<{
-    history: {
-      squares: string;
-    }[];
-    stepNumber: number;//いま何手目の状態を見ているのか
-    xIsNext: boolean;
-  } | null>(null); //初期値null
-
-  const handleClick = (i: number) => {
-    let c_history = 
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory(history_s.concat([{ squares: squares }])); //concat=元の配列をミューテートしないpush
+    setStepNumber(history_s.length);
+    setXIsNext(!xIsNext);
   };
 
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice(); //squares配列のコピーを作成
-    if (CalculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      //concat=元の配列をミューテートしないpush
-      history: history.concat([
-        {
-          squares: squares
-        }
-      ]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
-    });
-  }
+  const jumpTo = (step: number) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
 
-  jumpTo(step) {
-    this.setState({
-      //setState で直接指定されたプロパティのみを更新
-      stepNumber: step,
-      xIsNext: step % 2 === 0
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = CalculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
+  const current = history[stepNumber];
+  const winner = CalculateWinner(current.squares);
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)} //Boardに関数渡す
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
     );
-  }
-};
+  });
 
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i: number) => handleClick(i)} //Boardに関数渡す
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
 export default Game;
-*/
